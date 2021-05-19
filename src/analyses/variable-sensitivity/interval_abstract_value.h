46c4389e2e53ebc5f340e7376e822e1bedd8824f
Jez Higgins <jez@jezuk.co.uk>
2021-05-19 11:24:02 +0100

    interval_abstract_valuet::to_predicate

--- src/analyses/variable-sensitivity/interval_abstract_value.h
+++ src/analyses/variable-sensitivity/interval_abstract_value.h
@@ -92,6 +92,8 @@ protected:
   meet_with_value(const abstract_value_pointert &other) const override;
 
 private:
+  exprt to_predicate_internal(const exprt &name) const override;
+
   constant_interval_exprt interval;
 };
 
